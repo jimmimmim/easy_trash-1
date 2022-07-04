6b04541cc8a8dbb9be0b36ac62301de64b9239d6
JIYEONKIMPROG <88828307+JIYEONKIMPROG@users.noreply.github.com>
2022-07-04 21:13:56 +0900

    no connect screen

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -64,6 +64,30 @@ function MainScreen({ onPress, navigation }) {
       </View>
     );
 }
+//NoConnectScreen
+function NoConnectScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TouchableOpacity onPress={() => navigation.navigate('KakaoConnection')} style={styles.buttonBox_yellow}>
+          <Text style={styles.buttonText_small}>이전으로</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('KakaoConnection')} style={styles.buttonBox_yellow}>
+            <Text style={styles.buttonText_small}>화면 안내</Text>
+      </TouchableOpacity>
+      <Text style={styles.text_yellow}>안내 사항</Text>
+      <Text style={styles.text_yellow}>카카오 계정 연결 없이 사용하면{"\n"}이후 결제와 데이터 저장이 되지 않습니다.</Text>
+      <Text style={styles.text_yellow}>마이페이지에서 언제든지 연동할 수 있습니다.</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('KakaoConnection')} style={styles.buttonBox_yellow}>
+            <Text style={styles.buttonText_small}>사용하기</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('KakaoConnection')} style={styles.buttonBox_yellow}>
+            <Text style={styles.buttonText_small}>카카오계정 연결하기</Text>
+      </TouchableOpacity>
+   
+    </View>
+  );
+}
+
 
 function LoginScreen({ navigation }) {
     return (
