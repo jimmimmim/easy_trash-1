ac0e2cb5d88d822dfac32752771d57dc861f0b76
JIYEONKIMPROG <88828307+JIYEONKIMPROG@users.noreply.github.com>
2022-07-05 15:12:31 +0900

    stylesheet seperated

---
+++ project/src/component/LandingScreenStyle.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const LandingScreenStyle = StyleSheet.create({
+  text_style:{
+    marginTop:70,
+    marginBottom : 10
+  }
+});
+
+
+export default LandingScreenStyle;
