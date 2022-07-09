b62f9a00c672cefeea26a2fd20129598135428f0
jimmimmim <likecharry@gmail.com>
2022-07-09 19:12:12 +0900

    Fix dependency

--- project/src/pages/TutorialScreen.jsx
+++ project/src/pages/TutorialScreen.jsx
@@ -4,7 +4,7 @@ import layout from '../styles/Layout';
 import button from '../styles/Button';
 import text from '../styles/Text';
 
-import Tts from 'react-native-tts';
+// import Tts from 'react-native-tts';
 
 // 사용법
 function BigTrashScreen({ navigation }) {
