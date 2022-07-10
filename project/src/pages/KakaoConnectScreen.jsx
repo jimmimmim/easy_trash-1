0e9bbef9809841e48d0c660022ead4b56fbb292b
JIYEONKIMPROG <88828307+JIYEONKIMPROG@users.noreply.github.com>
2022-07-10 20:26:56 +0900

    before kakao login access code

--- project/src/pages/KakaoConnectScreen.jsx
+++ project/src/pages/KakaoConnectScreen.jsx
@@ -15,24 +15,13 @@ const kakaoOpt = {
 
 // //카카오 계정 연결하기 페이지
 function KakaoConnectScreen({ navigation }) {
-  function LogInProgress(data){
-    // access code는 url에 붙어 장황하게 날아온다.
-    // substringd으로 url에서 code=뒤를 substring하면 된다.
-    const exp = "code=";
-    var condition = target.indexOf(exp);
-    if(condition!= -1){
-        var request_code = target.substring(condition+exp.length);
-        console.log("access code :: " + request_code);
-      }
-    }
+  
   return (
     <View style={{flex: 1}}>
         <WebView
             originWhitelist={['*']}
             scalesPageToFit={false}
             source = {{uri : 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${kakaoOpt.clientId}&redirect_uri=${redirectUri}'}}
-            injectedJavaScript = {runFirst}
-            onMessage = {(event)=>{LogInProgress(event.nativeEvent["url"]);}}
             // onMessage ... :: webview에서 온 데이터를 event handler로 잡아서 logInProgress로 전달
         />
       </View>
